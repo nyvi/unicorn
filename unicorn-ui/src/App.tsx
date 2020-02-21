a4eeed1c9ddff8fb741a975099d2c5d51dd090fc
nyvi <kang.eternity@gmail.com>
2020-02-21 23:26:35 +0800

    feat():方法列表展示

--- unicorn-ui/src/App.tsx
+++ unicorn-ui/src/App.tsx
@@ -1,73 +1,78 @@
 import React, {Component} from 'react';
 import './App.css';
 
-import {Icon, Menu} from 'antd';
+import {Menu} from 'antd';
 
 const {SubMenu} = Menu;
 
 class App extends Component {
 
+    state = {
+        docMap: {}
+    };
+
+    componentDidMount() {
+        this.getDoc((res) => {
+            this.setState({docMap: res});
+        });
+    };
+
     handleClick = () => {
-        console.log('click ');
+        // this.getDoc();
+        console.log("点击了")
+    };
+
+    getDoc = (callBack) => {
+        let docUrl = "http://localhost:8888/api/api-docs";
+        fetch(docUrl, {
+            method: 'GET',
+            headers: new Headers({'Content-Type': 'application/json'})
+        }).then(
+            res => res.json().then((data) => {
+                callBack(data);
+            })
+        )
     };
 
     render() {
+        const {docMap} = this.state;
+
+        const loop = data => {
+            let ret = [];
+            for (let key in data) {
+                if (!data.hasOwnProperty(key)) continue;
+                let value = data[key];
+                let name = value.desc ? value.desc : value.name;
+                let api = [];
+                let apiList = value.apiList;
+                for (let i = 0; i < apiList.length; i++) {
+                    api.push(<Menu.Item key={apiList[i].path} title={apiList[i].desc}>{apiList[i].path}</Menu.Item>)
+                }
+                ret.push(<SubMenu title={name} key={key}>{api}</SubMenu>);
+            }
+            return ret;
+        };
+
         return (
             <Menu
                 onClick={this.handleClick}
                 style={{width: 256}}
-                defaultSelectedKeys={['1']}
-                defaultOpenKeys={['sub1']}
+                // defaultSelectedKeys={['1']}
+                // defaultOpenKeys={['sub1']}
                 mode="inline"
             >
-                <SubMenu
-                    key="sub1"
-                    title={
-                        <span>
-              <Icon type="mail"/>
-              <span>Navigation One</span>
-            </span>
-                    }
-                >
-                    <Menu.ItemGroup key="g1" title="Item 1">
-                        <Menu.Item key="1">Option 1</Menu.Item>
-                        <Menu.Item key="2">Option 2</Menu.Item>
-                    </Menu.ItemGroup>
-                    <Menu.ItemGroup key="g2" title="Item 2">
-                        <Menu.Item key="3">Option 3</Menu.Item>
-                        <Menu.Item key="4">Option 4</Menu.Item>
-                    </Menu.ItemGroup>
-                </SubMenu>
-                <SubMenu
-                    key="sub2"
-                    title={
-                        <span>
-              <Icon type="appstore"/>
-              <span>Navigation Two</span>
-            </span>
-                    }
-                >
-                    <Menu.Item key="5">Option 5</Menu.Item>
-                    <Menu.Item key="6">Option 6</Menu.Item>
-                    <SubMenu key="sub3" title="Submenu">
-                        <Menu.Item key="7">Option 7</Menu.Item>
-                        <Menu.Item key="8">Option 8</Menu.Item>
-                    </SubMenu>
-                </SubMenu>
-                <SubMenu
-                    key="sub4"
-                    title={
-                        <span>
-              <Icon type="setting"/>
-              <span>Navigation Three</span>
-            </span>
-                    }
-                >
-                    <Menu.Item key="9">Option 9</Menu.Item>
-                    <Menu.Item key="10">Option 10</Menu.Item>
-                    <Menu.Item key="11">Option 11</Menu.Item>
-                    <Menu.Item key="12">Option 12</Menu.Item>
-                </SubMenu>
+                {loop(docMap)}
+
+                {/*<SubMenu*/}
+                {/*    key="sub1"*/}
+                {/*    title="啥"*/}
+                {/*>*/}
+                {/*    <Menu.Item key="g1" title="Item 1">*/}
+                {/*        Hello*/}
+                {/*    </Menu.Item>*/}
+                {/*</SubMenu>*/}
+
+
             </Menu>
         );
     }
