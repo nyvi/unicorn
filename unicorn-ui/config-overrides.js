43e6971dc537e2ffc54ab10de10171a0e0560442
nyvi <kang.eternity@gmail.com>
2020-02-26 22:19:20 +0800

    feat():静态资源路径

--- unicorn-ui/config-overrides.js
+++ unicorn-ui/config-overrides.js
@@ -4,8 +4,8 @@ const {override, fixBabelImports} = require('customize-cra');
 const buildPath = () => (config, env) => {
     const path = require('path');
     const paths = require('react-scripts/config/paths');
-    paths.appBuild = path.join(path.dirname(paths.appBuild), 'src/main/resources/META-INF');
-    config.output.path = path.join(path.dirname(config.output.path), 'src/main/resources/META-INF');
+    paths.appBuild = path.join(path.dirname(paths.appBuild), 'src/main/resources/META-INF/resources');
+    config.output.path = path.join(path.dirname(config.output.path), 'src/main/resources/META-INF/resources');
     return config;
 };
 
