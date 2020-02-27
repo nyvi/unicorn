dcaca457278b532f7dd8fd9d1ce96097690a067e
nyvi <kang.eternity@gmail.com>
2020-02-27 22:54:34 +0800

    feat():解决本地启动问题

--- unicorn-ui/config-overrides.js
+++ unicorn-ui/config-overrides.js
@@ -2,10 +2,12 @@ const {override, fixBabelImports} = require('customize-cra');
 
 
 const buildPath = () => (config, env) => {
-    const path = require('path');
-    const paths = require('react-scripts/config/paths');
-    paths.appBuild = path.join(path.dirname(paths.appBuild), 'src/main/resources/META-INF/resources');
-    config.output.path = path.join(path.dirname(config.output.path), 'src/main/resources/META-INF/resources');
+    if (env === 'production') {
+        const path = require('path') || "";
+        const paths = require('react-scripts/config/paths');
+        paths.appBuild = path.join(path.dirname(paths.appBuild), 'src/main/resources/META-INF/resources');
+        config.output.path = path.join(path.dirname(config.output.path), 'src/main/resources/META-INF/resources');
+    }
     return config;
 };
 
